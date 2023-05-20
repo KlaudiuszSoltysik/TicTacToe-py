adf64e3940aa673373372937f5f6bdc13a389d88
Klaudiusz Sołtysik <klaudiusz.s1405@gmail.com>
2023-05-20 23:19:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,85 @@
 from machine import Pin
 import time
+import math
 
 from SR import SR
+
+
+def check_win():
+    if any(field is None for field in board):
+        return None
+    elif board[0] == 'x' and board[1] == 'x' and board[2] == 'x' or board[3] == 'x' and board[4] == 'x' and board[5] == 'x' or board[6] == 'x' and board[7] == 'x' and board[8] == 'x' or board[0] == 'x' and board[3] == 'x' and board[6] == 'x' or board[1] == 'x' and board[4] == 'x' and board[7] == 'x' or board[2] == 'x' and board[5] == 'x' and board[8] == 'x' or board[0] == 'x' and board[4] == 'x' and board[8] == 'x' or board[2] == 'x' and board[4] == 'x' and board[6] == 'x':
+        return -10
+    elif board[0] == 'o' and board[1] == 'o' and board[2] == 'o' or board[3] == 'o' and board[4] == 'o' and board[5] == 'o' or board[6] == 'o' and board[7] == 'o' and board[8] == 'o' or board[0] == 'o' and board[3] == 'o' and board[6] == 'o' or board[1] == 'o' and board[4] == 'o' and board[7] == 'o' or board[2] == 'o' and board[5] == 'o' and board[8] == 'o' or board[0] == 'o' and board[4] == 'o' and board[8] == 'o' or board[2] == 'o' and board[4] == 'o' and board[6] == 'o':
+        return 10
+    else:
+        return 0
+
+
+def minimax(board, is_maxing):
+    result = check_win()
+
+    if result is not None:
+        return result
+    
+    if is_maxing:
+        best_score = -math.inf
+
+        for i in range(len(board)):
+            if board[i] is None:
+                board[i] = 'o'
+                score = minimax(board, False)
+                board[i] = None
+
+                if score > best_score:
+                    best_score = score
+
+    else:
+        best_score = math.inf
+
+        for i in range(len(board)):
+            if board[i] is None:
+                board[i] = 'x'
+                score = minimax(board, True)
+                board[i] = None
+
+                if score < best_score:
+                    best_score = score
+    
+    return best_score
+
+
+def light_diodes():
+    diodes_green.clear()
+    number = (
+        x_fields[-2] * 1
+        + x_fields[-3] * 2
+        + x_fields[-4] * 4
+        + x_fields[-5] * 8
+        + x_fields[-6] * 16
+        + x_fields[-7] * 32
+        + x_fields[-8] * 64
+        + x_fields[-9] * 128
+    )
+    diodes_green.bits(number, 8, True)
+
+    if x_fields[-1]:
+        diode_8_green.value(1)
+
+    number = (
+        o_fields[-2] * 1
+        + o_fields[-3] * 2
+        + o_fields[-4] * 4
+        + o_fields[-5] * 8
+        + o_fields[-6] * 16
+        + o_fields[-7] * 32
+        + o_fields[-8] * 64
+        + o_fields[-9] * 128
+    )
+    diodes_red.bits(number, 8, True)
+
+    if o_fields[-1]:
+        diode_8_red.value(1)
 
 
 # DEFINE PINS
@@ -35,6 +113,7 @@ diode_8_green = Pin(2, Pin.OUT)
 
 player_move = True
 game_loop = False
+board = [None, None, None, None, None, None, None, None, None]
 x_fields = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 o_fields = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -42,32 +121,38 @@ o_fields = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 while True:
     if player_move:
         for i, button in enumerate(control_buttons):
-            if button.value() and not x_fields[i]:
+            if button.value() and not x_fields[i] and not o_fields[i]:
                 game_loop = True
-                # player_move = False
+                player_move = False
                 x_fields[i] = 1
+                board[i] = 'x'
 
-                diodes_green.clear()
-                number = (
-                    x_fields[-2] * 1
-                    + x_fields[-3] * 2
-                    + x_fields[-4] * 4
-                    + x_fields[-5] * 8
-                    + x_fields[-6] * 16
-                    + x_fields[-7] * 32
-                    + x_fields[-8] * 64
-                    + x_fields[-9] * 128
-                )
-                diodes_green.bits(number, 8, True)
-
-                if x_fields[-1]:
-                    diode_8_green.value(1)
+                light_diodes()
     else:
         # IMPOSSIBLE DIFFICULTY
         if difficulty_switch:
-            pass
+
+            best_score = -math.inf
+            best_move = None
+
+            for i in range(len(board)):
+                if board[i] is None:
+                    board[i] = 'o'
+                    score = minimax(board, False)
+                    board[i] = None
+
+                    if score > best_score:
+                        best_score = score
+                        best_move = i
+
+            o_fields[best_move] = 1
+            board[best_move] = 'o'
+            player_move = True
+
+            light_diodes()
+
         # HARD DIFFICULTY
         else:
             pass
 
-    time.sleep(0.2)
+    time.sleep(0.1)
